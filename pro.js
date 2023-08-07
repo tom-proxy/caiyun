72be0a55f7faabf000e2e8aebaa67844168d5194
湯姆 <89062432+tom-proxy@users.noreply.github.com>
2023-08-07 19:38:45 +0800

    Update pro.js

--- pro.js
+++ pro.js
@@ -4,30 +4,30 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 if (url.includes("/v1/vip_info")) {
   if (obj.vip) {
-    obj.vip.expires_time = "8000000000";
+    obj.vip.expires_time = "8888888888";
   }
   if (obj.svip) {
-    obj.svip.expires_time = "8000000000";
+    obj.svip.expires_time = "8888888888";
   }
 } else if (url.includes("/v2/user")) {
   if (obj.result) {
     obj.result.svip_given = 888;
     obj.result.is_phone_verified = true;
     obj.result.is_xy_vip = true;
-    obj.result.vip_expired_at = 8000000000;
+    obj.result.vip_expired_at = 8888888888;
     obj.result.is_vip = true;
-    obj.result.xy_svip_expire = 8000000000;
+    obj.result.xy_svip_expire = 8888888888;
     if (obj.result.wt) {
       if (obj.result.wt.vip) {
         obj.result.wt.vip.enabled = true;
-        obj.result.wt.vip.expired_at = 8000000000;
-        obj.result.wt.vip.svip_expired_at = 8000000000;
+        obj.result.wt.vip.expired_at = 8888888888;
+        obj.result.wt.vip.svip_expired_at = 8888888888;
       }
       obj.result.wt.svip_given = 888;
     }
     obj.result.is_primary = true;
-    obj.result.xy_vip_expire = 8000000000;
-    obj.result.svip_expired_at = 8000000000;
+    obj.result.xy_vip_expire = 8888888888;
+    obj.result.svip_expired_at = 8888888888;
     obj.result.vip_type = "s";
   }
 }
